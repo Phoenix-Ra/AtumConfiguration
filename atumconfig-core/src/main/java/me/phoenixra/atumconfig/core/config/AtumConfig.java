7a07a5fcd3f4b4178f77a9af1f491fee933e7c6f
Bogdan <53151806+Phoenix-Ra@users.noreply.github.com>
2026-02-25 06:32:13 +0400

    version 1.0.1

--- atumconfig-core/src/main/java/me/phoenixra/atumconfig/core/config/AtumConfig.java
+++ atumconfig-core/src/main/java/me/phoenixra/atumconfig/core/config/AtumConfig.java
@@ -85,8 +85,8 @@ public class AtumConfig implements Config {
 
     @Override
     public void set(@NotNull String path, @Nullable Object obj) {
-        if((obj instanceof Config config) && !(config instanceof AtumConfigSection)){
-            obj = new AtumConfigSection(config);
+        if((obj instanceof Config) && !(obj instanceof AtumConfigSection)){
+            obj = new AtumConfigSection((Config)obj);
         }
         String nearestPath = path.split("\\.")[0];
         if(path.contains(".")){
